559ffd4c5b2c65099cfbc87880e3b0766cbfb316
Dmitriy Kotov <ikot@mail.ru>
2025-06-03 15:54:55 +0300

    make

---
+++ Src/istockexchange.cpp
@@ -0,0 +1,31 @@
+#include "StockExchange/istockexchange.h"
+
+#ifndef QT_NO_SSL
+
+using namespace TradingCatCommon;
+using namespace StockExchange;
+
+IStockExchange::IStockExchange(const TradingCatCommon::StockExchangeID &stockExchangeId, QObject* parent /* = nullptr */)
+    : QObject{parent}
+    , _stockExchangeId(stockExchangeId)
+{
+    Q_ASSERT(!stockExchangeId.isEmpty());
+
+    qRegisterMetaType<TradingCatCommon::StockExchangeID>("TradingCatCommon::StockExchangeID");
+}
+
+const TradingCatCommon::StockExchangeID &IStockExchange::id() const
+{
+    return _stockExchangeId;
+}
+
+void IStockExchange::start()
+{
+}
+
+void IStockExchange::stop()
+{
+    emit finished();
+}
+
+#endif
